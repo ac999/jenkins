cb502efccd2abc26e615da3781c331aa01ea1e8a
Andrei C <44598788+ac999@users.noreply.github.com>
2020-10-20 19:43:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -i --rm -v "$PWD":/home/groovy/scripts -w /home/groovy/scripts groovy groovy tictactoe.groovy
+docker run -i --rm -v "$PWD":/home/groovy/scripts -w /home/groovy/scripts groovy groovy tictactoe.groovy test
